1eab363eede6f59997e70115e2ee8d4421e43b06
Morshed <morshednayeem@icloud.com>
2022-10-15 23:44:57 +0600

    third commit

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.dispatch import receiver
+
+from django.contrib.auth.models import User
+
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASECADE, related_name="profile")
+    full_name = models.CharField(max_length=100,blank=True,null=True)
+    address = models.TextField(max_length=300,blank=True,null=True)
+    country = models.CharField(max_length=100,blank=True,null=True)
+    full_name = models.CharField(max_length=100,blank=True,null=True)
+    full_name = models.CharField(max_length=100,blank=True,null=True)
+    
